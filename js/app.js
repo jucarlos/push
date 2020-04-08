d271da2327afc146bd57edac134ffdc04d62c8cc
Carlos <jcarlosjccm@gmail.com>
2020-04-08 10:24:53 +0200

    Cambiamos ruta sw

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 (function () {
-    'use strict';
+    
+    let url = window.location.href;
+
+    let swLocation = '/push/sw.js';
 
     // Verificar si podemos usar sw
     if ( navigator.serviceWorker ){
-        navigator.serviceWorker.register('/sw.js');
+
+        if ( url.includes('localhost') ) {
+            swLocation = '/sw.js';
+        }
+        navigator.serviceWorker.register(swLocation);
     }
 
     
