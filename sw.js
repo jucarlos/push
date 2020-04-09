9fc3cabfafb3933766b376a2b6637579aad01041
Carlos <jcarlosjccm@gmail.com>
2020-04-09 19:39:17 +0200

    Mas opciones en la notificacion

--- sw.js
+++ sw.js
@@ -34,20 +34,72 @@
       const opciones = {
         body: data.cuerpo,
         icon: 'images/icons/icon-72x72.png',
-        badge: 'img/badgejccm.png',
-        imagen: 'img/papascomunica.jpg',
+        badge: 'img/badgeFondoBlanco.png',
+        image: 'img/papascomunica.jpg',
         vibrate: [500,110,500,110,450,110,200,110,170,40,450,110,200,110,170,40,500],
-        openUrl: '/'
+        openUrl: '/',
+        data: {
+           // url: 'https://google.com',
+            url: '/',
+            id: '1234',
+        },
+        // actions : [
+        //     {
+        //         action: 'actualiar',
+        //         title: 'Actualizar',
+        //         icon: 'img/badgeFondoBlanco.png'
+        //     },
+        //     {
+        //         action: 'eliminar',
+        //         title: 'Eliminar',
+        //         icon: 'img/badgeFondoBlanco.png'
+        //     }
+        // ]
 
 
       };
 
-
-        
-
       e.waitUntil( self.registration.showNotification( title, opciones ));
 
     });
+
+    // Cuando se cierra la notificación
+    self.addEventListener('notificationclose', e => {
+
+
+
+    });
+
+    // Cuando se hace click en la notificación
+    self.addEventListener('notificationclick', e => {
+
+        const notificacion = e.notification;
+        const accion  = e.action;
+        
+        // console.log({ notificacion, accion } );
+
+        const respuesta = clients.matchAll()
+        .then( clientes => {
+
+            let cliente = clientes.find( c => {
+                return c.visibilityState === 'visible';
+            });
+
+            
+            if ( cliente !== undefined ) {
+                cliente.navigate( notificacion.data.url);
+                cliente.focus();
+            } else {
+                clients.openWindow( notificacion.data.url );
+            }
+            
+            return notificacion.close();
+        });
+
+        e.waitUntil ( respuesta );
+
+    });
+
 
     
 
